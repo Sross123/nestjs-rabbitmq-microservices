62807233ec171180c443e1e964aa2082c1bcb99d
Shashi <shashiross9@gmail.com>
2026-07-19 12:55:25 +0530

    Add RabbitMQ data files and feature flags for enhanced functionality
    - Created .erlang.cookie for RabbitMQ authentication.
    - Added feature flags to support various RabbitMQ features including quorum queues and detailed queue endpoints.
    - Initialized Mnesia database files for RabbitMQ coordination and message stores.
    - Configured vhost settings and queue metadata for improved message handling.
    - Included WAL and snapshot files for quorum and coordination to ensure data consistency and recovery.

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    // Registering the microservice client within the NestJS IoC Container
+    ClientsModule.register([
+      {
+        name: 'ORDER_SERVICE', // The injection token used in our controllers
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://guest:guest@localhost:5672'],
+          queue: 'order_queue',
+          queueOptions: {
+            durable: true,
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
